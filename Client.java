7f51e495977dfdbed63da2cd00554a88d31cb9e6
Carol Lu <Carol10051113@gmail.com>
2025-11-16 18:10:38 -0500

    set up

--- Client.java
+++ Client.java
@@ -1,3 +1,24 @@
+import java.net.Socket;
+
 public class Client {
 
+    public Client(String host, int port) throws Exception{
+
+    }
+
+    public Socket getSocket(){
+
+    }
+
+    public void handshake() throws Exception{
+
+    }
+
+    public String request(String number) throws Exception{
+
+    }
+
+    public void disconnect() throws Exception{
+        
+    }
 }
--- Server.java
+++ Server.java
@@ -1,3 +1,42 @@
+import java.net.Socket;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+private class ClientHandler implements Runnable {
+
+    private ClientHandler(Socket socket){
+
+    }
+
+    @Override
+    public void run(){
+
+    }
+}
+
 public class Server {
 
+    public Server(int port) throws Exception{
+
+    }
+
+    public void serve(int expectedClients) throws Exception{
+
+    }
+
+    public ArrayList<LocalDateTime> getConnectedTimes(){
+
+    }
+
+    public void disconnect() throws Exception{
+
+    }
+
+    private void handleClient(Socket sock){
+
+    }
+
+    private int countFactors(long n){
+
+    }
 }
