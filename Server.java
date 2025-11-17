a779b07f435120589c6fc056cc321aa7f89956f2
Carol Lu <Carol10051113@gmail.com>
2025-11-17 01:05:53 -0500

    server

--- Server.java
+++ Server.java
@@ -1,7 +1,11 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Collections;
 
 private class ClientHandler implements Runnable {
 
@@ -27,11 +31,27 @@ public class Server {
     }
 
     public void serve(int client) throws Exception{
+        for (int i = 0; i < client; i++) {
+            Socket socket = serverSocket.accept();
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter out = new PrintWriter(socket.getOutputStream());
 
+            String key = in.readLine();
+            if (!key.equals("12345")) {
+                out.println("couldn't handshake");
+                out.flush();
+                socket.close();
+                continue;
+            }
+            connectedTimes.add(LocalDateTime.now());
+           
+        }
     }
 
     public ArrayList<LocalDateTime> getConnectedTimes(){
-
+        ArrayList<LocalDateTime> copy = new ArrayList<>(connectedTimes);
+        Collections.sort(copy);
+        return copy;
     }
 
     public void disconnect() throws Exception{
@@ -42,11 +62,4 @@ public class Server {
         }
     }
 
-    private void handleClient(Socket sock){
-
-    }
-
-    private int countFactors(long n){
-
-    }
 }
